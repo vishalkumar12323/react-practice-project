805d7cb0d49ec56fa542df94f98df9b3e84de3ae
Vishal Ninaniya <137691442+vishalkumar12323@users.noreply.github.com>
2024-07-09 12:47:07 +0000

    add dummy data from dummyJosn api

--- custom-pagination/src/App.jsx
+++ custom-pagination/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState, useEffect } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
 
+  const fetchProducts = async () => {
+    const res = await fetch(`https://dummyjson.com/products`);
+    const data = await res.json();
+    setProducts(data.products);
+  };
+
+  const handlePagination = () => {};
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        {products.length > 0 &&
+          products.slice(0, 5).map((pro, i) => (
+            <div key={pro.id} className="products">
+              <img src={pro.thumbnail} alt={pro.title} />
+              <span>{pro.title}</span>
+            </div>
+          ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="buttons">
+        <ul>
+          <li onClick={() => handlePagination()}>◀</li>
+          <li onClick={() => handlePagination()}>▶</li>
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
